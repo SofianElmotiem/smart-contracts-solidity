fee6b645793e172a38dde24e2b1965e7e93e39c6
Sofian Elmotiem <164787469+SofianElmotiem@users.noreply.github.com>
2024-05-21 10:32:11 +0200

    Save Gas

---
+++ GasSaving.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.8.1;
+contract SaveGas {
+    uint8 resultA = 0;
+    uint256 resultB = 0;
+
+    function UseUint() external returns (uint256) {
+        uint256 selectedRange = 50;
+        for (uint256 i = 0; i < selectedRange; i++) {
+            resultB += 1;
+        }
+        return resultB;
+    }
+
+    function UseUInt8() external returns (uint8) {
+        uint8 selectedRange = 50;
+        for (uint8 i = 0; i < selectedRange; i++) {
+            resultA += 1;
+        }
+        return resultA;
+    }
+}
